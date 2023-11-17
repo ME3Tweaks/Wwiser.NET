50adc8b89f0888aacbf48e1d62131ad27fc38bc8
henbagle <ben.hagle@gmail.com>
2023-11-17 18:34:36 -0500

    Add v56 test for bank header parsing

--- Wwiser.NET.Tests/BankHeaderTests.cs
+++ Wwiser.NET.Tests/BankHeaderTests.cs
@@ -16,6 +16,22 @@ public class BankHeaderTests
         Assert.AreEqual(134, chunk.BankGeneratorVersion);
         Assert.AreEqual(7572, chunk.ProjectId);
     }
+    
+    [Test]
+    public void V56_Parses()
+    {
+        var data = TestData.GetTestDataBytes(@"BankHeader\v56.bin");
+        var serializer = new BinarySerializer();
+        var result = serializer.Deserialize<ChunkContainer>(data, new BankSerializationContext(134));
+        
+        Assert.AreEqual("BKHD", result.Tag);
+        Assert.AreEqual(0x18, result.ChunkSize);
+        Assert.IsInstanceOf<BankHeader>(result.Chunk);
+        
+        var chunk = result.Chunk as BankHeader;
+        Assert.AreEqual(56, chunk.BankGeneratorVersion);
+        Assert.AreEqual( 1564500913, chunk.SoundBankId);
+    }
 
     [Test]
     public void V134_Reserializes()
