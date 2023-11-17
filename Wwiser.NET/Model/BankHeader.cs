f2757e779acc7c669aa616c4641b99beeb356514
henbagle <ben.hagle@gmail.com>
2023-11-17 18:12:47 -0500

    Finish bank header for most modern versions? Add xmldoc comments

--- Wwiser.NET/Model/BankHeader.cs
+++ Wwiser.NET/Model/BankHeader.cs
@@ -56,41 +56,97 @@ namespace ME3Tweaks.Wwiser.Model
     public class BankHeader : Chunk
     {
         public override string Tag => @"BKHD";
-
+        
+        // TODO: Implement version <= 26 of bank header CAkBankMgr__ProcessBankHeader
+        
+        /// <summary>
+        /// Version of Wwise used to generate bank
+        /// </summary>
         [FieldOrder(0)]
         public uint BankGeneratorVersion { get; set; }
 
+        /// <summary>
+        /// Sound bank ID
+        /// </summary>
         [FieldOrder(1)]
         public uint SoundBankId { get; set; }
 
+        /// <summary>
+        /// Language ID enum value
+        /// (&lt;= v122)
+        /// </summary>
         [FieldOrder(2)]
         [SerializeWhen(nameof(BankGeneratorVersion), 122, ComparisonOperator.LessThanOrEqual)]
         public LanguageID LanguageId { get; set; }
         
+        /// <summary>
+        /// String hash of language ID
+        /// (&gt; v122)
+        /// </summary>
         [FieldOrder(3)]
         [SerializeWhen(nameof(BankGeneratorVersion), 122, ComparisonOperator.GreaterThan)]
         public uint LanguageIdStringHash { get; set; }
         
+        /// <summary>
+        /// Unknown - potentially timestamp?
+        /// (&lt;= v26)
+        /// </summary>
         [FieldOrder(4)]
         [SerializeWhen(nameof(BankGeneratorVersion), 26, ComparisonOperator.LessThanOrEqual)]
         public UInt64 Timestamp { get; set; }
         
+        /// <summary>
+        /// Bool of feedback in bank
+        /// (v27 &lt;= version &gt;= v126)
+        /// </summary>
         [FieldOrder(5)]
         [SerializeWhen(nameof(BankGeneratorVersion), true, 
             ConverterType = typeof(BetweenConverter), 
             ConverterParameter = new[] {27, 126})]
-        public bool FeedbackInBank { get; set; }
+        public UInt32 FeedbackInBank { get; set; }
 
+        /// <summary>
+        /// Bitpacked bools encoded in AltValues depending on version
+        /// </summary>
         [FieldOrder(6)]
         [SerializeWhen(nameof(BankGeneratorVersion), 126, ComparisonOperator.GreaterThan)]
         public uint AltValues { get; set; }
+        
+        //TODO: Implement bit packed bools in AltValues
+        // elif version <= 134:
+        //    obj.U32('uAltValues') \
+        //    .bit('bUnused', obj.lastval, 0, 0xFFFF) \
+        //    .bit('bDeviceAllocated', obj.lastval, 16, 0xFFFF)
+        // else:
+        //    obj.U32('uAltValues') \
+        //    .bit('uAlignment', obj.lastval, 0, 0xFFFF) \
+        //    .bit('bDeviceAllocated', obj.lastval, 16, 0xFFFF)
 
+        /// <summary>
+        /// ID of project
+        /// (&gt; v76)
+        /// </summary>
         [FieldOrder(7)]
         [SerializeWhen(nameof(BankGeneratorVersion), 76, ComparisonOperator.GreaterThan)]
         public uint ProjectId { get; set; } = 0;
         
+        /// <summary>
+        /// Type of bank
+        /// (&gt; v141)
+        /// </summary>
         [FieldOrder(8)]
         [SerializeWhen(nameof(BankGeneratorVersion), 141, ComparisonOperator.GreaterThan)]
         public AkBankType SoundBankType { get; set; }
+        
+        /// <summary>
+        /// Unknown hash of bank?
+        /// (&amp;gt; v141)
+        /// </summary>
+        [FieldOrder(9)]
+        [FieldCount(16)]
+        [SerializeWhen(nameof(BankGeneratorVersion), 141, ComparisonOperator.GreaterThan)]
+        public sbyte[] BankHash { get; set; }
+        
+        // TODO: Copy any padding over into a byte[]? Will allow proper reserialization.
     }
 }
